3cee88bef41f9895450a4ff0fb8fd6118db8927f
Shota Taniguchi <st11121.grtkmch@gmail.com>
2024-03-10 23:17:14 +0900

    create counter

---
+++ src/practice/4/counter.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+export const Counter: React.FC = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const decrement = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+};
